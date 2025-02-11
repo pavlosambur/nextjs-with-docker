9863c44ab99cdc3eeb2925b80833aaa40c37fb29
Pavlo <samburp@gmail.com>
2025-02-11 22:39:19 +0200

    :fire: works

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
         <Image className="dark:invert" src="/next.svg" alt="Next.js logo" width={180} height={38} priority />
         <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
           <li className="mb-2">
-            Get started by editing kjklj{" "}
+            Get started by editing{" "}
             <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">app/page.tsx</code>.
           </li>
           <li>Save and see your changes instantly.</li>
